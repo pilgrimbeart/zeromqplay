0b1b049398fdbb020d5035569e4a85d524ec51b3
Pilgrim Beart <pilgrim@beart.org.uk>
2013-01-24 15:42:48 +0000

    Made simpler for timing packet rates

--- server1.py
+++ server1.py
@@ -10,10 +10,8 @@ socket.bind("tcp://*:5556")
 pktnum = 0
 
 while True:
-	zipcode = random.randrange(1,100000)
-
 	if(pktnum%10000==0):
-		print "Sending packet",pktnum, zipcode
-	socket.send("%d %d %d" % (zipcode, 1, 1))
+		print "Sending packet",pktnum
+	socket.send("%d" % (pktnum))
 	pktnum+=1
 
